ec98fa2bdd224330e38961ae58b0e37c481e71b6
pwade3 <philippwade3@gmail.com>
2019-10-19 13:41:45 -0400

    Update json.js

--- json.js
+++ json.js
@@ -4,7 +4,7 @@ $().ready(function(){
         for(i in data.Kits)
         {
             var num = i;
-            num+=2;
+            num = parseInt(num)+parseInt(2);
             $("#kits").append('<h2>'+data.Kits[i].name+'</h2>');
             $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="images/renders/'+data.Kits[i].image.name+'">');
             $("#kits").append('<p>'+data.Kits[i].text+'</p>');
