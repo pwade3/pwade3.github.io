4fa94ea92f0c5d38ec88ebb22c436343e0410b3c
pwade3 <philippwade3@gmail.com>
2019-10-19 12:58:20 -0400

    Update json.js

--- json.js
+++ json.js
@@ -1,6 +1,6 @@
 $().ready(function(){
     $.getJSON("data.json",function(data){
         console.log(data);
-        $("#img").html('<img src="images/renders/'+data.Renders[0].name+'" >');
+        $("#img").html('<img class="kitRender hover-shadow" src="images/renders/'+data.Renders[0].name+'" >');
     });
 });
