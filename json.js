2bbc49a101550717737a922b086839d01af07a4d
pwade3 <philippwade3@gmail.com>
2019-10-19 18:06:25 -0400

    Update json.js

--- json.js
+++ json.js
@@ -1,16 +1,20 @@
 $().ready(function(){
     $.getJSON("data.json",function(data){
         console.log(data);
+        
+        //Bulding all the Kit listings
         for(i in data.Kits)
         {
             var num = i;
             num = parseInt(num)+parseInt(2);
             $("#kits").append('<h2>'+data.Kits[i].kitname+'</h2>');
             $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
+            $("#kits").append('<br>');
             $("#kits").append('<p>'+data.Kits[i].text+'</p>');
             $("#kits").append('<br>');
         }
 
+        //Building the table of renders
         var table = "<table><tr>"
         for(i = 1; i < data.Renders.length; i++)
         {
@@ -26,5 +30,67 @@ $().ready(function(){
         }
         table+="</tr></table>";
         $("#renderGallery").append(table);
+
+
+        //Building the gallery
+        var modalContent='<div class="modal-content">';
+        var slideCount = data.Renders.length+data.Kits.length;
+
+        modalContent+='<div class="mySlides">';
+        modalContent+='<div class="numbertext"> 1 / ' + slideCount + '</div>';
+        modalContent+='<img src=images/renders'+data.Renders[0].name+'" style="width:100%"';
+        modalContent+='</div>';
+
+        console.log(slideCount)
+        for(i in data.Kits)
+        {
+            modalContent+='<div class="mySlides">';
+            var slideNum = i;
+            slideNum = parseInt(slideNum)+parseInt(1);
+            modalContent+='<div class="numbertext">'+ slideNum + ' / ' + slideCount + '</div>';
+            modalContent+='<img src=images/renders/'+data.Kits[i].image.name+'" style="width:100%"';
+            modalContent+='</div>';
+        }
+        for(i in data.Renders)
+        {
+            modalContent+='<div class="mySlides">';
+            var slideNum = i;
+            slideNum = parseInt(slideNum)+parseInt(6);
+            modalContent+='<div class="numbertext">'+ slideNum + ' / ' + slideCount + '</div>';
+            modalContent+='<img src=images/renders/'+data.Renders[i].name+'" style="width:100%"';
+            modalContent+='</div>';
+        }
+
+        //prev/next controls
+        modalContent+='<a class="prev" onclick="plusSlides(-1)">&#10094;</a> <a class="next" onclick="plusSlides(1)">&#10095;</a>';
+        
+        //caption text
+        modalContent+='<div class="caption-container"> <p id="caption"></p> </div>';
+
+        //thumbnail image controls
+        modalContent+='<div class="column">';
+        modalContent+='<img class="demo" src=images/renders'+data.Renders[0].name+'" onclick="currentSlide(1)" alt = "'+data.Renders[0].desc+'">';
+        modalContent+='</div>';
+
+        for(i in data.Kits)
+        {
+            modalContent+='<div class="column">';
+            var slideNum = i;
+            slideNum = parseInt(slidenum)+parseInt(1);
+            modalContent+='<img class="demo" src=images/renders'+data.Kits[i].image.name+'" onclick="currentSlide('+slideNum+') alt="'+data.Kits[i].image.desc+'">';
+            modalContent+='</div>';
+        }
+        for(i in data.Renders)
+        {
+            modalContent+='<div class="column">';
+            var slideNum = i;
+            slideNum = parseInt(slidenum)+parseInt(6);
+            modalContent+='<img class="demo" src=images/renders'+data.Renders[i].name+'" onclick="currentSlide('+slideNum+') alt="'+data.Renders[i].desc+'">';
+            modalContent+='</div>';
+        }
+        modalContent+='</div';
+
+        console.log(modalContent);
+
     });
 });
