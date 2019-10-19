5e806c58d4e7902776f6c8793e7b33cd0b2a0582
pwade3 <philippwade3@gmail.com>
2019-10-19 17:09:28 -0400

    Update json.js

--- json.js
+++ json.js
@@ -8,6 +8,23 @@ $().ready(function(){
             $("#kits").append('<h2>'+data.Kits[i].kitname+'</h2>');
             $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
             $("#kits").append('<p>'+data.Kits[i].text+'</p>');
+            $("#kits").append('<br>');
         }
+
+        $("#renderGallery").append("<table><tr>");
+        for(i in data.Renders)
+        {
+            var num = i;
+            num = parseInt(num)+parseInt(7);
+            $("#renderGallery").append("<td>");
+            $("#renderGallery").append('<img class="boardRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="images/renders/'+data.Renders[i].name+'" alt="'+data.Renders[i].alt+'">');
+            $("#renderGallery").append("</td>");
+            if(parseInt(i)%3 == 0)
+            {
+                $("#renderGallery").append("</tr>");
+            }
+        }
+        $("#renderGallery").append("</tr></table>");
+
     });
 });
