90027d4694baa0526bfe6afb961a9edd72daf0a9
pwade3 <philippwade3@gmail.com>
2019-10-19 12:52:31 -0400

    Update json.js

--- json.js
+++ json.js
@@ -1,6 +1,6 @@
 $().ready(function(){
     $.getJSON("data.json",function(data){
         console.log(data);
-        $("#img").html('<img src="'+data.Renders[0].name+'" >');
+        $("#img").html('<img src=images/renders/"'+data.Renders[0].name+'" >');
     });
 });
