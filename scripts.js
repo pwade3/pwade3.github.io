2d4749a63881fee4fabff55118a24499bec09140
pwade3 <philippwade3@gmail.com>
2019-10-19 19:31:24 -0400

    asdfasdf

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/*function openModal() {
+function openModal() {
   document.getElementById("myModal").style.display = "block";
   document.getElementById("myModal").focus();
 }
@@ -19,22 +19,22 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
 
 document.getElementById("myModal").addEventListener("keydown", function(event){
     //do something on keydown
@@ -49,4 +49,4 @@ document.getElementById("myModal").addEventListener("keydown", function(event){
             closeModal();
             break;
     }
-});*/
+});
