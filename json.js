3df1aae9d5d5595ea877a0ea2fef60e82cccae71
pwade3 <philippwade3@gmail.com>
2019-10-19 13:12:49 -0400

    json testing

--- json.js
+++ json.js
@@ -1,6 +1,10 @@
 $().ready(function(){
     $.getJSON("data.json",function(data){
         console.log(data);
+        for(i in data.Kits)
+        {
+            $("#kits").html('<p>'+data.Kits[i].text+'</p>');
+        }
         $("#img").html('<img class="kitRender hover-shadow" src="images/renders/'+data.Renders[0].name+'" >');
     });
 });
