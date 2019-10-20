a3b691149d6c3c41e65c997ecd3e7026551210a8
pwade3 <philippwade3@gmail.com>
2019-10-19 23:43:25 -0400

    Update adPageBuilder.js

--- AnalogDreams/adPageBuilder.js
+++ AnalogDreams/adPageBuilder.js
@@ -8,7 +8,7 @@ $().ready(function(){
             var num = i;
             num = parseInt(num)+parseInt(2);
             $("#kits").append('<h2>'+data.Kits[i].kitname+'</h2>');
-            $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="/AnalogDreams/images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
+            $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="AnalogDreams/images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
             $("#kits").append('<br>');
             $("#kits").append('<p>'+data.Kits[i].text+'</p>');
             $("#kits").append('<br>');
@@ -21,7 +21,7 @@ $().ready(function(){
             var num = i;
             num = parseInt(num)+parseInt(6);
             table+="<td>";
-            table+='<img class="boardRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="/AnalogDreams/images/renders/'+data.Renders[i].name+'" alt="'+data.Renders[i].alt+'">';
+            table+='<img class="boardRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="AnalogDreams/images/renders/'+data.Renders[i].name+'" alt="'+data.Renders[i].alt+'">';
             table+="</td>";
             if(parseInt(i)%3 == 0)
             {
@@ -38,17 +38,16 @@ $().ready(function(){
 
         modalContent+='<div class="mySlides">';
         modalContent+='<div class="numbertext"> 1 / ' + slideCount + '</div>';
-        modalContent+='<img src="/AnalogDreams/images/renders/'+data.Renders[0].name+'" style="width:100%">';
+        modalContent+='<img src="AnalogDreams/images/renders/'+data.Renders[0].name+'" style="width:100%">';
         modalContent+='</div>';
 
-        console.log(slideCount)
         for(i in data.Kits)
         {
             modalContent+='<div class="mySlides">';
             var slideNum = i;
             slideNum = parseInt(slideNum)+parseInt(2);
             modalContent+='<div class="numbertext">'+ slideNum + ' / ' + slideCount + '</div>';
-            modalContent+='<img src="/AnalogDreams/images/renders/'+data.Kits[i].image.name+'" style="width:100%">';
+            modalContent+='<img src="AnalogDreams/images/renders/'+data.Kits[i].image.name+'" style="width:100%">';
             modalContent+='</div>';
         }
         for(i = 1; i < data.Renders.length; i++)
@@ -57,7 +56,7 @@ $().ready(function(){
             var slideNum = i;
             slideNum = parseInt(slideNum)+parseInt(6);
             modalContent+='<div class="numbertext">'+ slideNum + ' / ' + slideCount + '</div>';
-            modalContent+='<img src="/AnalogDreams/images/renders/'+data.Renders[i].name+'" style="width:100%">';
+            modalContent+='<img src="AnalogDreams/images/renders/'+data.Renders[i].name+'" style="width:100%">';
             modalContent+='</div>';
         }
 
@@ -69,7 +68,7 @@ $().ready(function(){
 
         //thumbnail image controls
         modalContent+='<div class="column">';
-        modalContent+='<img class="demo" src="/AnalogDreams/images/renders/thumbs/'+data.Renders[0].thumbnail+'" onclick="currentSlide(1)" alt = "'+data.Renders[0].desc+'">';
+        modalContent+='<img class="demo" src="AnalogDreams/images/renders/thumbs/'+data.Renders[0].thumbnail+'" onclick="currentSlide(1)" alt = "'+data.Renders[0].desc+'">';
         modalContent+='</div>';
 
         for(i in data.Kits)
@@ -77,7 +76,7 @@ $().ready(function(){
             modalContent+='<div class="column">';
             var slideNum = i;
             slideNum = parseInt(slideNum)+parseInt(2);
-            modalContent+='<img class="demo" src="/AnalogDreams/images/renders/thumbs/'+data.Kits[i].image.thumbnail+'" onclick="currentSlide('+slideNum+')" alt="'+data.Kits[i].image.desc+'">';
+            modalContent+='<img class="demo" src="AnalogDreams/images/renders/thumbs/'+data.Kits[i].image.thumbnail+'" onclick="currentSlide('+slideNum+')" alt="'+data.Kits[i].image.desc+'">';
             modalContent+='</div>';
         }
         for(i = 1; i < data.Renders.length; i++)
@@ -85,7 +84,7 @@ $().ready(function(){
             modalContent+='<div class="column">';
             var slideNum = i;
             slideNum = parseInt(slideNum)+parseInt(6);
-            modalContent+='<img class="demo" src="/AnalogDreams/images/renders/thumbs/'+data.Renders[i].thumbnail+'" onclick="currentSlide('+slideNum+')" alt="'+data.Renders[i].desc+'">';
+            modalContent+='<img class="demo" src="AnalogDreams/images/renders/thumbs/'+data.Renders[i].thumbnail+'" onclick="currentSlide('+slideNum+')" alt="'+data.Renders[i].desc+'">';
             modalContent+='</div>';
         }
         modalContent+='</div>';
