e3fba35a06bfa79ed0f0b0c3d191b29743d2d875
pwade3 <philippwade3@gmail.com>
2019-10-20 02:43:54 -0400

    asdf

--- Projects/AnalogDreams/adPageBuilder.js
+++ Projects/AnalogDreams/adPageBuilder.js
@@ -8,7 +8,7 @@ $().ready(function(){
             var num = i;
             num = parseInt(num)+parseInt(2);
             $("#kits").append('<h2>'+data.Kits[i].kitname+'</h2>');
-            $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="/Project/AnalogDreams/images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
+            $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="/Projects/AnalogDreams/images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
             $("#kits").append('<br>');
             $("#kits").append('<p>'+data.Kits[i].text+'</p>');
             $("#kits").append('<br>');
@@ -21,7 +21,7 @@ $().ready(function(){
             var num = i;
             num = parseInt(num)+parseInt(6);
             table+="<td>";
-            table+='<img class="boardRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="images/renders/'+data.Renders[i].name+'" alt="'+data.Renders[i].alt+'">';
+            table+='<img class="boardRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="/Projects/AnalogDreams/images/renders/'+data.Renders[i].name+'" alt="'+data.Renders[i].alt+'">';
             table+="</td>";
             if(parseInt(i)%3 == 0)
             {
