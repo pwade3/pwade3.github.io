451e1d90c24f8ed14a36517f59862aa07bab1035
pwade3 <philippwade3@gmail.com>
2019-10-19 21:44:48 -0400

    style

--- json.js
+++ json.js
@@ -7,7 +7,7 @@ $().ready(function(){
         {
             var num = i;
             num = parseInt(num)+parseInt(2);
-            $("#kits").append('<h3>'+data.Kits[i].kitname+'</h3>');
+            $("#kits").append('<h2>'+data.Kits[i].kitname+'</h2>');
             $("#kits").append('<img class="kitRender hover-shadow" onclick="openModal();currentSlide('+num+')" src="images/renders/'+data.Kits[i].image.name+'" alt="'+data.Kits[i].image.alt+'">');
             $("#kits").append('<br>');
             $("#kits").append('<p>'+data.Kits[i].text+'</p>');
