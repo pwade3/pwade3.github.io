a6fa9cc11129238234bf23b23aba05a0fe9c9240
pwade3 <philippwade3@gmail.com>
2019-10-20 03:35:20 -0400

    Update adScripts.js

--- Projects/AnalogDreams/adScripts.js
+++ Projects/AnalogDreams/adScripts.js
@@ -23,7 +23,7 @@ function closeModal() {
 }
 
 var slideIndex = 1;
-document.addEventListener("DOMContentLoaded",() => {showSlides(slideIndex);
+showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -50,4 +50,3 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
-});
